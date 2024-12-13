97313e3fe8e2615e1f6e397ea16196448157a145
Franz Decinal <dfranzzzzz05@gmail.com>
2024-12-14 04:53:37 +0800

    Organized initial folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import SignInForm from "./_auth/forms/SignInForm";
+import SignUpForm from "./_auth/forms/SignUpForm";
+
 import "./globals.css";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/*public routes*/}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+        {/*private routes*/}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
---
+++ src/_auth/forms/SignInForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignInForm = () => {
+  return <div>SignIn</div>;
+};
+
+export default SignInForm;
